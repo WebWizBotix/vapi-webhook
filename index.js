d3d3e67214fce1a17434e5eddb235db6f7614641
WebWizBotix <webwizbotix@gmail.com>
2025-12-27 16:07:41 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+{
+  "name": "vapi-webhook",
+  "version": "1.0.0",
+  "description": "Webhook receiver for Vapi that forwards to Make",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "body-parser": "^1.20.2",
+    "node-fetch": "^2.6.7"
+  }
+}
